2f7ab178fa86c8c48b0ddbcc75fc89c02f711f26
Devgroup <marcus.ekstrom@devgroup.se>
2021-03-28 14:22:26 +0200

    Rename variables

--- db/db.py
+++ db/db.py
@@ -1,53 +1,53 @@
 import dbAccessor
 from datetime import datetime
 
-getUserQuery = "SELECT username FROM user WHERE username = (%s)"
-insertUserQuery = "INSERT INTO user (name) VALUES (%s)"
-addRunkQuery = "INSERT INTO runk (name) VALUES (%s)"
-getRunksHighscoreQuery = "SELECT COUNT(created), username FROM runk GROUP BY username"
-addCommandQuery = "INSERT INTO command (command, msg) VALUES (%s, %s)"
-getCommandQuery = "SELECT msg FROM command WHERE phrase = (%s)"
-getAllCommandsQuery = "SELECT phrase FROM command"
-delCommandQuery = "DELETE FROM command where phrase = (%s)"
+__getUserQuery = "SELECT username FROM user WHERE username = (%s)"
+__insertUserQuery = "INSERT INTO user (name) VALUES (%s)"
+__addRunkQuery = "INSERT INTO runk (name) VALUES (%s)"
+__getRunksHighscoreQuery = "SELECT COUNT(created), username FROM runk GROUP BY username"
+__addCommandQuery = "INSERT INTO command (command, msg) VALUES (%s, %s)"
+__getCommandQuery = "SELECT msg FROM command WHERE phrase = (%s)"
+__getAllCommandsQuery = "SELECT phrase FROM command"
+__delCommandQuery = "DELETE FROM command where phrase = (%s)"
 
 def saveUser(name):
   connection = dbAccessor.db()
-  connection.query(insertUserQuery, (name), True)
+  connection.query(__insertUserQuery, (name), True)
 
 def getUser(name):
   connection = dbAccessor.db()
-  return connection.query(getUserQuery, (name), False, True)
+  return connection.query(__getUserQuery, (name), False, True)
 
 def addRunk(name):
   connection = dbAccessor.db()
-  user = connection.query(getUserQuery, (name), False, True)
+  user = connection.query(__getUserQuery, (name), False, True)
   if not user:
-    connection.query(insertUserQuery, (name), True)
+    connection.query(__insertUserQuery, (name), True)
   timestamp = datetime.today().strftime('%Y-%m-%d')
   params = (name, timestamp)
-  execution = connection.query(addRunkQuery, params, True)
+  execution = connection.query(__addRunkQuery, params, True)
   print(execution)
   return execution
 
 def getRunkHighscore():
   connection = dbAccessor.db()
-  execution = connection.query(getRunksHighscoreQuery)
+  execution = connection.query(__getRunksHighscoreQuery)
   print(execution)
   return execution
 
 def addCommand(phrase, msg):
   connection = dbAccessor.db()
   params = (phrase, msg)
-  connection.query(addCommandQuery, params, True)
+  connection.query(__addCommandQuery, params, True)
 
 def getCommand(phrase):
   connection = dbAccessor.db()
-  return connection.query(getCommandQuery, (phrase), False, True)
+  return connection.query(__getCommandQuery, (phrase), False, True)
 
 def listCommands():
   connection = dbAccessor.db()
-  return connection.query(getAllCommandsQuery)
+  return connection.query(__getAllCommandsQuery)
 
 def delCommand(phrase):
   connection = dbAccessor.db()
-  return connection.query(delCommandQuery, (phrase), True)
+  return connection.query(__delCommandQuery, (phrase), True)
