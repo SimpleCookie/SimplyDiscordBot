96da855f39b0d6ac7a9cdba5bb6aba91b4a066bb
Devgroup <marcus.ekstrom@devgroup.se>
2021-04-01 13:47:32 +0200

    stashed stuff

--- db/create_db.sql
+++ db/create_db.sql
@@ -3,17 +3,17 @@ create database simply_discord_bot;
 use simply_discord_bot;
 
 CREATE TABLE user (
-    username varchar(36) not null primary key,
+    username varchar(36) not null primary key
 );
 
 CREATE TABLE runk (
     username varchar(36) not null,
     created timestamp not null,
     primary key (username, created),
-    foreign key (username) references user (username),
+    foreign key (username) references user (username)
 );
 
 CREATE TABLE command (
     phrase varchar(36) not null primary key,
-    msg varchar(256) not null,
+    msg varchar(256) not null
 );
